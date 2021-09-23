e73132edad5c5f2a1bf935af5a6f863849dae56d
Héctor Manuel Muñoz Flores <skyspartanblack@gmail.com>
2021-09-23 11:33:47 -0500

    first commit, initializing the proyect

---
+++ src/controllers/indexController.js
@@ -0,0 +1,68 @@
+const connection = require('../database')
+let dateObject = new Date()
+const controller = {}
+
+controller.test = (req, res)=>{
+    res.json({Test:"Hi, im working"})
+}
+
+controller.ListAll = async(req, res)=>{
+    const results = await connection.query("Select * from notes")
+    res.json(results)
+}
+controller.ListOne = async(req, res)=>{
+    const {idnote} = req.params;
+    const results = await connection.query(`select * from notes where idnote=${idnote}`)
+    if(results.length > 0){
+        res.json(results)
+    }else{
+        res.json({results:"Not found"})
+    }
+}
+
+controller.Save = async(req, res)=>{
+    const {title, content, importance} = req.body
+    const createdat = (dateObject.getDate()+"/"+"0"+dateObject.getMonth()+"/"+dateObject.getFullYear())
+    const fkuser = 1
+    const newNote = {title, content,createdat,fkuser,importance}
+    try {
+        await connection.query("insert into notes set ?",[newNote])
+        res.status(200).json({insert:"succesfull"})
+        
+    } catch (error) {
+        console.log(error)
+        res.status(400).json({insert:"something wen't wrong"})
+    }
+   
+} 
+
+controller.Update=async(req, res)=>{
+    const {idnote} = req.params
+    console.log(idnote)
+    const {title, content, importance} = req.body;
+    const updatedNote = {title, content,importance}
+    try {
+        await connection.query("update notes set ? where idnote = ?",[updatedNote,idnote])
+        res.status(200).json({infoupdate: "succesfull"})
+    } catch (error) {
+        console.log(error)
+    }
+
+    
+}
+controller.Delete=async(req,res)=>{
+    const {idnote} = req.params
+    try {
+
+        await connection.query("delete from notes where idnote = ?",[idnote])
+        res.status(200).json({deletion:"succesfull"})
+        
+    } catch (error) {
+        console.log(error)
+        res.status(400).json({insert:"something wen't wrong"})
+    }
+
+}
+
+
+module.exports = controller
---
+++ src/index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const cors = require('cors')
+const morgan = require('morgan')
+const app = express()
+
+//initialazing database connection
+const initDatabase = require("./database")
+
+//using middlewares
+app.use(cors())
+app.use(morgan('dev'))
+app.use(express.urlencoded({extended:false}))
+app.use(express.json())
+
+//using the routes
+app.use('/', require("./routes/indexRoutes.js"))
+
+//initialazing the server
+app.listen(port = 4000 || process.env.PORT,()=>{
+    console.log(`Listening on port ${port}`)
+})
